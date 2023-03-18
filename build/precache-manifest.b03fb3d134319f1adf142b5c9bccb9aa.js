77af056ccfb317ef4fab45325b8e4859d9f87aa3
ZawHtet-246098 <zhtet1636@gmail.com>
2023-03-18 11:03:00 +0630

    final-commit

---
+++ build/precache-manifest.b03fb3d134319f1adf142b5c9bccb9aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bb545916bea21082caa20a6b7e486e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3ed67c28d857eab7e8",
+    "url": "/static/css/2.c33191e4.chunk.css"
+  },
+  {
+    "revision": "c4bf0dddd79b78fbaaaa",
+    "url": "/static/css/main.de017258.chunk.css"
+  },
+  {
+    "revision": "7a3ed67c28d857eab7e8",
+    "url": "/static/js/2.04e8b819.chunk.js"
+  },
+  {
+    "revision": "ad41a80b0e957371c2602bebf5a71d90",
+    "url": "/static/js/2.04e8b819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bf0dddd79b78fbaaaa",
+    "url": "/static/js/main.4ac9b847.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.4ac9b847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "469199dfe890eb9359cfe4aabec8eba0",
+    "url": "/static/media/logo-removebg-preview.469199df.png"
+  },
+  {
+    "revision": "4d73fe3339a04e7677788599af2189f1",
+    "url": "/static/media/press-banner.4d73fe33.png"
+  }
+]);
